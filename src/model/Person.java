edb5398ce133ed5547d45cc4dc880c71bc9be455
Diego Hidalgo Lopez <d13g0fhl@gmail.com>
2021-04-01 04:19:32 -0500

    Added the Person class

---
+++ src/model/Person.java
@@ -0,0 +1,29 @@
+package model;
+
+public class Person {
+
+    private String idNumber; //Identification number of the person
+    private IdType idType; //Identification type of the person
+
+    public Person(String idNumber, IdType idType) {
+        this.idNumber = idNumber;
+        this.idType = idType;
+    }//End constructor
+
+    public void setIdNumber(String idNumber) {
+        this.idNumber = idNumber;
+    }//End setIdNumber
+
+    public String getIdNumber() {
+        return idNumber;
+    }//End getIdNumber
+
+    public void setIdType(IdType idType) {
+        this.idType = idType;
+    }//End setIdType
+
+    public IdType getIdType() {
+        return idType;
+    }//End getIdType
+
+}//End class Person
